e81d967b473fca73f381ab2c9c5a3908cd5ea423
OctoFort <fortionfortion@gmail.com>
2024-11-11 14:37:59 +0500

    changed title of index

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Document</title>
+    <title>Loh.kz</title>
     <script src="https://cdn.tailwindcss.com"></script>
 {{--    <script src="{{ asset('js/js.js') }}" defer></script>--}}
     @vite('resources/css/app.css')
